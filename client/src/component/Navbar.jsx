2796f44fdbf5e425a7e997d3f77f4283322c8a2f
mayahNeko <mayahibela@gmail.com>
2025-08-09 08:09:42 +0300

    fix: Navbar color

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="bg-red-600 p-4 text-white text-center text-xl font-bold">
+    <nav className="bg-red-500 p-3 text-white text-center text-l font-bold">
       Marvel Heroes
     </nav>
   );
